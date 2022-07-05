bd49fe5bc04eca2af80519f863268933b02699c9
alianrock <654849917@qq.com>
2022-07-05 21:37:27 +0800

    init

--- src/App.js
+++ src/App.js
@@ -3,20 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <img className="app-header-logo" src="/static/img/logo.e4a996db.png"/>
+        <h1 className="app-header-title">茫洋</h1>
       </header>
     </div>
   );
